28a1e379a4db46fa8f919b8301e8cd1a82483e70
wojciechkepka <wojtek.kepka@protonmail.com>
2020-05-12 16:45:22 +0200

    Add rx/tx sampling

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod out;
 mod tests;
 mod utils;
 use anyhow::{anyhow, Result};
+use async_std::fs::read_to_string;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::default::Default;
@@ -15,6 +16,8 @@ use std::process::Command;
 use std::str;
 use std::str::FromStr;
 use std::string::String;
+use std::thread;
+use std::time::Duration;
 
 fn handle<T: Default, E: Display + Debug>(result: Result<T, E>) -> T {
     match result {
@@ -69,6 +72,9 @@ pub enum Memory {
     MemFree,
 }
 
+// (rx, tx) b/s
+type Transfer = (f64, f64);
+
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
 pub struct NetworkDevice {
     name: String,
@@ -76,6 +82,56 @@ pub struct NetworkDevice {
     transfered_bytes: u64,
     ipv4_addr: Ipv4Addr,
     ipv6_addr: Ipv6Addr,
+}
+impl NetworkDevice {
+    pub fn new(name: &str) -> Self {
+        NetworkDevice {
+            name: name.to_string(),
+            received_bytes: 0,
+            transfered_bytes: 0,
+            ipv4_addr: Ipv4Addr::UNSPECIFIED,
+            ipv6_addr: Ipv6Addr::UNSPECIFIED,
+        }
+    }
+    // Returns a tuple of u64 bytes per second over interval time duration
+    pub async fn get_rx_tx_persec(&self, interval: std::time::Duration) -> Result<Transfer> {
+        let mut first_rx = 0.;
+        let mut last_rx = 0.;
+        let mut first_tx = 0.;
+        let mut last_tx = 0.;
+        let mut current_rx;
+        let mut current_tx;
+        let mut secs = interval.as_secs();
+        loop {
+            let net_dev = read_to_string(SysProperty::NetDev.path()).await?;
+            let re = Regex::new(
+                r"([\d\w]*):\s*(\d*)\s*\d*\s*\d*\s*\d*\s*\d*\s*\d*\s*\d*\s*\d*\s*(\d*)",
+            )?;
+            for network_dev in re.captures_iter(&net_dev) {
+                if self.name == network_dev[1].to_string() {
+                    if secs == interval.as_secs() {
+                        first_rx = network_dev[2].parse::<f64>()?;
+                        first_tx = network_dev[3].parse::<f64>()?;
+                    } else {
+                        last_rx = network_dev[2].parse::<f64>()?;
+                        last_tx = network_dev[3].parse::<f64>()?;
+                    }
+                    break;
+                }
+            }
+
+            current_rx = (last_rx - first_rx) / (interval.as_secs() - secs) as f64;
+            current_tx = (last_tx - first_tx) / (interval.as_secs() - secs) as f64;
+
+            if secs <= 0 {
+                break;
+            }
+            secs -= 1;
+
+            thread::sleep(Duration::new(1, 0));
+        }
+        Ok((current_rx, current_tx))
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
